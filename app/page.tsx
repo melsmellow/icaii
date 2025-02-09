d2256b58b549d3037f21a318b7d014ce16c8da4c
Mel Carlo <melcarlo.iguis@gmail.com>
2025-02-09 21:04:28 +0800

    adjust height

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { DatePickerDemo } from "@/modules/Datepicker";
 export default function Home() {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <div className="h-[60vh] w-[90vw]">
+      <div className="h-[70vh] w-[90vw]">
         <DatePickerDemo />
       </div>
     </div>
